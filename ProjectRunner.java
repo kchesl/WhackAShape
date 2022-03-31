26e07e2d86f650cc10e10f6a3e4c1187a8223abe
kchesl <102710433+kchesl@users.noreply.github.com>
2022-03-30 23:24:46 -0400

    Update ProjectRunner.java

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -1,17 +1,4 @@
-package game;
 
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Kirsten Chesley (kchesley888)
-
-/**
- *
- * @author Kirsten Chesley (kchesley888)
- * @version (2020-Mar-01)
- */
 public class ProjectRunner {
 
     /**

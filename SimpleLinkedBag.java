951764e22a95b60486627375e1ff2db6b2e6ce70
kchesl <102710433+kchesl@users.noreply.github.com>
2022-03-30 23:25:41 -0400

    Update SimpleLinkedBag.java

--- SimpleLinkedBag.java
+++ SimpleLinkedBag.java
@@ -1,10 +1,4 @@
 package game;
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Kirsten Chesley (kchesley888)
 
 import bag.Node;
 import bag.SimpleBagInterface;
@@ -12,8 +6,7 @@ import student.TestableRandom;
 
 /**
  *
- * @author Kirsten Chesley (kchesley888)
- * @version (2020-Mar-01)
+
  * @param <T>
  *            the entry
  */

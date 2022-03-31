c082b758b5811af4bfa9318851727706be1d6328
kchesl <102710433+kchesl@users.noreply.github.com>
2022-03-30 23:25:08 -0400

    Update SimpleArrayBag.java

--- SimpleArrayBag.java
+++ SimpleArrayBag.java
@@ -1,21 +1,8 @@
-package game;
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Kirsten Chesley (kchesley888)
 
 import bag.SimpleBagInterface;
 import student.TestableRandom;
 
-/**
- *
- * @author Kirsten Chesley (kchesley888)
- * @version (2020-Mar-01)
- * @param <T>
- *            the entry
- */
+
 public class SimpleArrayBag<T> implements SimpleBagInterface<T> {
 
     private T[] bag;
